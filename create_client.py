7c3524445a31588da3df55449f2c47e06cffa491
Geoffrey Kleinberg <gkleinberg21@gmail.com>
2025-02-04 11:25:20 -0500

    Allow the user to interact with OpenSearch locally
    If the hostname is set to localhost, a different client will be created that allows the user
    to interact with a local instance of OpenSearch.

--- create_client.py
+++ create_client.py
@@ -11,6 +11,27 @@ def create_client():
     port = os.getenv('OPENSEARCH_PORT')
     region = 'us-east-1'
 
+    if host is None or port is None:
+        raise ValueError('Please set the environment variables OPENSEARCH_HOST and OPENSEARCH_PORT')
+    
+    if host == 'localhost':
+        auth = ('admin', os.getenv('OPENSEARCH_INITIAL_PASSWORD'))
+
+        ca_certs_path = certifi.where()
+        # Create the client with SSL/TLS enabled, but hostname verification disabled.
+        client = OpenSearch(
+            hosts = [{'host': host, 'port': port}],
+            http_compress = True, # enables gzip compression for request bodies
+            http_auth = auth,
+            use_ssl = True,
+            verify_certs = False,
+            ssl_assert_hostname = False,
+            ssl_show_warn = False,
+            ca_certs = ca_certs_path
+        )
+
+        return client
+    
     service = 'aoss'
     credentials = boto3.Session().get_credentials()
     auth = AWSV4SignerAuth(credentials, region, service)
